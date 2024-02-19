028b69bb0ca479f8c869b51a6f6d1ee3811c688d
haifeng.jiang <haifeng.jiang@JiangdeMacBook-Pro.local>
2024-02-19 15:00:12 +0800

    some refactoring

--- app/src/main/java/com/example/bullsandcows/MainActivity.kt
+++ app/src/main/java/com/example/bullsandcows/MainActivity.kt
@@ -6,17 +6,17 @@ import android.util.Pair
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
     private lateinit var candidate: TextView
     private var disabledButtons = hashSetOf<Button>()
     private var nList = mutableListOf<TextView>()
     private var bList = mutableListOf<TextView>()
     private var cList = mutableListOf<TextView>()
     private var round = 0
-    private var target = newTarget()
+    private var targetNumber = generateNewNumber()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -59,7 +59,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         cList.add(5, findViewById(R.id.c6))
     }
 
-    private fun newTarget(): String {
+    private fun newGame() {
+        targetNumber = generateNewNumber()
+        enableAll()
+        clearAll()
+        candidate.text = ""
+        round = 0
+    }
+
+    private fun generateNewNumber(): String {
         val numbers = (0..9).toList()
         val result = numbers.shuffled()
         val ans =
@@ -68,7 +76,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         return ans
     }
 
-    private fun isNumber(id: Int): Boolean {
+    private fun isNumberBtn(id: Int): Boolean {
         val idSet = hashSetOf(
             R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
             R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9
@@ -76,8 +84,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         return idSet.contains(id)
     }
 
+    private fun isClearBtn(id: Int): Boolean {
+        return id == R.id.btnCE
+    }
+
+    private fun isSubmitBtn(id: Int): Boolean {
+        return id == R.id.btnGO
+    }
+
     private fun enableAll() {
         disabledButtons.forEach { it.isEnabled = true }
+    }
+
+    private fun disableBtn(btn: Button) {
+        btn.isEnabled = false
+        disabledButtons.add(btn)
     }
 
     private fun clearAll() {
@@ -90,40 +111,55 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         var bulls = 0
         var all = 0
         for (i in 0..3) {
-            if (target[i] == guess[i]) bulls++
-            if (target.contains(guess[i])) all++
+            if (targetNumber[i] == guess[i]) bulls++
+            if (targetNumber.contains(guess[i])) all++
         }
 
-        return Pair(bulls, all-bulls)
+        return Pair(bulls, all - bulls)
+    }
+
+    private fun showAlertDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("GO ON") { _, _ -> newGame() }
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 
     override fun onClick(v: View?) {
         val btn = v as Button
-        if (isNumber(v.id)) {
+        if (isNumberBtn(v.id)) {
             if (candidate.length() < 4) {
                 candidate.append(btn.text)
-                btn.isEnabled = false
-                disabledButtons.add(btn)
+                disableBtn(btn)
             }
-        } else if (v.id == R.id.btnCE) {
+        } else if (isClearBtn(v.id)) {
             candidate.text = ""
             enableAll()
-        } else if (v.id == R.id.btnGO) {
+        } else if (isSubmitBtn(v.id)) {
             if (candidate.length() == 4 && round < 6) {
                 nList[round].text = candidate.text
                 val pair = countBullsCows(candidate.text.toString())
                 bList[round].text = pair.first.toString()
                 cList[round].text = pair.second.toString()
+
+                if (pair.first == 4) {
+                    showAlertDialog("Great", "哇，答对了，你太棒了")
+                    return
+                }
                 enableAll()
                 round++
+                if (round == 6) {
+                    showAlertDialog("Sorry", "正确答案是$targetNumber，再接再厉哦")
+                    return
+                }
                 candidate.text = ""
             }
         } else if (v.id == R.id.btnNew) {
-            target = newTarget()
-            enableAll()
-            clearAll()
-            candidate.text = ""
-            round = 0
+            newGame()
         }
     }
+
+
 }
